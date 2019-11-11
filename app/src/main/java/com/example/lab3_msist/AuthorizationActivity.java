6e1d28e7cc335c8e61b044e7a597c3837dbcb623
Vladislav001 <VladGurjev@mail.ru>
2019-11-11 21:13:40 +0400

    сообщение о неверном пароле

--- app/src/main/java/com/example/lab3_msist/AuthorizationActivity.java
+++ app/src/main/java/com/example/lab3_msist/AuthorizationActivity.java
@@ -1,9 +1,5 @@
 package com.example.lab3_msist;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,8 +9,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
-import java.util.Objects;
 
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
@@ -29,7 +30,7 @@ public class AuthorizationActivity extends AppCompatActivity {
     private EditText password;
     private Button login;
     private TextView mTextViewResult;
-    public String url= "https://lab1msist.herokuapp.com/profile";
+    public String url= "https://lab1msist.herokuapp.com/user/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +41,22 @@ public class AuthorizationActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.edit_password);
         login = (Button) findViewById(R.id.button_login);
         mTextViewResult = findViewById(R.id.text_view_result);
-
-        final Request request = new Request.Builder()
-                .url(url).get()
-                .build();
-        new AsyncHttpRequest().execute(request);
-
-
     }
 
 
     // Обрабатываем нажатие кнопки "Войти":
     public void Login(View view) {
 
+        RequestBody formBody = new FormBody.Builder()
+                .add("email", email.getText().toString())
+                .add("password", password.getText().toString())
+                .build();
+
+        final Request request = new Request.Builder()
+                .url(url)
+                .post(formBody)
+                .build();
+        new AsyncHttpRequest().execute(request);
 
         // Если введенные логин и пароль будут словом "admin",
         // показываем Toast сообщение об успешном входе:
@@ -97,6 +101,47 @@ public class AuthorizationActivity extends AppCompatActivity {
 //        }
 //    }
 
+    public class AsyncHttpRequest extends AsyncTask<Request, Void, Response> {
+        @Override
+        protected Response doInBackground(Request... requests) {
+            Response response = null;
+            try {
+                OkHttpClient client = new OkHttpClient();
+                response = client.newCall(requests[0]).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Response response) {
+            super.onPostExecute(response);
+            try {
+
+                String resStr = response.body().string().toString();
+                try {
+                    JSONObject json = new JSONObject(resStr);
+                    JSONArray jsonArrayErrors = json.getJSONArray("errors");
+                    JSONObject error = new JSONObject(jsonArrayErrors.get(0).toString());
+
+                    if(error.length() > 0)
+                    {
+                        Toast.makeText(getApplicationContext(), error.get("message").toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                   // Log.d("myJson", error.get("message").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+              // Log.d("myRes", "ad");
+                //txtRequest.setText(response.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
 
